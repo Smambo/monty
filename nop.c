20bd89240aecf33417c115921f05c548b5e15c17
MphoCM <christian.mofokeng@gmai.com>
2023-08-23 14:49:45 -0700

    updated

--- nop.c
+++ nop.c
@@ -1,8 +1,11 @@
 #include "monty.h"
 /**
+  *execute - executes opcodes
   *f_nop- nothing
-  *@head: stack head
+  *@head: head f stack
+  *@file: monty file
   *@counter: line_number
+  *@content: line input
   *Return: no return
  */
 void f_nop(stack_t **head, unsigned int counter)
--- swap.c
+++ swap.c
@@ -2,7 +2,7 @@
 /**
  * f_swap - adds the top two elements of the stack.
  * @head: stack head
- * @counter: line_number
+ * @counter: line number
  * Return: no return
 */
 void f_swap(stack_t **head, unsigned int counter)
@@ -19,9 +19,9 @@ void f_swap(stack_t **head, unsigned int counter)
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
+		fclose(file);
+		free(content);
+		_free(*head);
 		exit(EXIT_FAILURE);
 	}
 	h = *head;
