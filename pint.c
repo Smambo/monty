3e57d574946ad3c316647566c7232bccaf05824d
Simamnkele <smambo85@gmail.com>
2023-08-23 23:30:40 +0200

    Added pint & pop opcode files

---
+++ pint.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * pint - prints value at the top of the stack
+ * @stack: head of stack
+ * @line_number: stack line number
+ * Return: nothing
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		fclose(sq.file);
+		free(sq.content);
+		_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
---
+++ pop.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * pop - removes top element of the stack
+ * @stack: head of stack
+ * @line_number: stack line number
+ * Return: nothing
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fclose(sq.file);
+		free(sq.content);
+		_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+	new = *stack;
+	*stack = new->next;
+	free(new);
+}
