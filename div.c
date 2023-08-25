37e67a67daa3e3fa82075fa92ae63e863317e43b
MphoCM <christian.mofokeng@gmai.com>
2023-08-25 14:04:39 -0700

    updated

--- div.c
+++ div.c
@@ -1,36 +1,26 @@
+#include "monty.h"
+
+/**
+ * _div - divides the top two elements of the stack.
+ * @head: stack of head
  * @line_number: line_number
  * Return: no return
-*/
-void div(stack_t **head, unsigned int line_number)
+ */
+void _div(stack_t **head, unsigned int line_number)
 {
-	stack_t *h;
-	int len = 0, aux;
+	int div;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
+	if((*head) == NULL || (*head)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
-		fclose(sq.file);
-		free(sq.content);
-		_free(*head);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
-	if (h->n == 0)
+	if((*head)->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
-		fclose(sq.file);
-		free(sq.content);
-		_free(*head);
 		exit(EXIT_FAILURE);
 	}
-	aux = h->next->n / h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+	div = (*head)->next->n / (*head)->n;
+	pop(head, line_number);
+	(*head)->n = div;
 }
---
+++ mul.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * mul - multiply the top two elements of the stack.
+ * @head: stack of head
+ * @line_number: line_number
+ * Return: no return
+ */
+void mul(stack_t **head, unsigned int line_number)
+{
+	int mul;
+
+	if((*head) == NULL || (*head)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	mul = (*head)->next->n * (*head)->n;
+	pop(head, line_number);
+	(*head)->n = mul;
+}
+
